f3df67304d9a310a7862df2b4345e172bb7edd84
BTAO TEAM <36434065+VijayanagaramRahul@users.noreply.github.com>
2020-10-09 22:27:00 +0530

    Update LoginValidationDetails.sql

--- MYSQL TABLE SCHEMAS/LoginValidationDetails.sql
+++ MYSQL TABLE SCHEMAS/LoginValidationDetails.sql
@@ -36,19 +36,7 @@ CREATE TABLE `LoginValidationDetails` (
   `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `course` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `LoginValidationDetails`
---
-
-INSERT INTO `LoginValidationDetails` (`userid`, `name`, `password`, `role`, `status`, `course`) VALUES
-('111', 'RAHUL', '698d51a19d8a121ce581499d7b701668', 'student', 'not verified', 'practical c programming'),
-('123', '123', '202cb962ac59075b964b07152d234b70', 'student', 'not verified', ''),
-('143', '143', '903ce9225fca3e988c2af215d4e544d3', 'student', 'not verified', 'Web Designing'),
-('admin', 'WireXTechnologies', '5f4dcc3b5aa765d61d8327deb882cf99', 'Admin', 'not verified', ''),
-('mvc ', 'mb', '444aaff69c70f0c299474a911fdb1a89', 'student', 'not verified', ''),
-('rahull', 'rahull', '671400d5f1f202dcdac3dbaf216bae8e', 'student', 'not verified', 'Web Designing');
-
+ 
 --
 -- Indexes for dumped tables
 --
